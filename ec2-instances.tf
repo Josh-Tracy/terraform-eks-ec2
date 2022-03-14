51e2c32663f113b402e4bdea5ce924292f1979ba
Joshua Tracy <joshuatracy@joshuatracy-v1fw0q7cvw.house.com>
2022-03-14 12:20:09 -0400

    Fixed subnets variable

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2_instances_ue1a" {
   key_name          = each.value["key_name"]
   tags              = each.value["tags"]
   associate_public_ip_address = true
-  subnet_id = data.aws_subnet.dev1-subnet.id
+  subnet_id = aws_subnet.dev1-subnet.id
   vpc_security_group_ids   = [aws_security_group.allow-web-traffic.id]
 
 }
@@ -20,7 +20,7 @@ resource "aws_instance" "ec2_instances_ue1b" {
   key_name          = each.value["key_name"]
   tags              = each.value["tags"]
   associate_public_ip_address = true
-  subnet_id = data.aws_subnet.dev2-subnet.id
+  subnet_id = aws_subnet.dev2-subnet.id
   vpc_security_group_ids   = [aws_security_group.allow-web-traffic.id]
 
 }
