82f4a7bc5c2e64e2177087114ce80d35d6f4f265
Joshua Tracy <joshuatracy@joshuatracy-v1fw0q7cvw.house.com>
2022-03-14 12:20:35 -0400

    added official eks module

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "dev-cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "dev-cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
@@ -13,23 +13,54 @@ provider "kubernetes" {
   #load_config_file       = false
 }
 
-module "my-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "dev-cluster"
-  cluster_version = "1.21"
-  subnets         = [aws_subnet.dev1-subnet.id, aws_subnet.dev2-subnet.id]
-  vpc_id          = aws_vpc.dev-vpc.id
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                    = "my-cluster"
+  cluster_version                 = "1.21"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  vpc_id     = aws_vpc.dev-vpc.id
+  subnet_ids = [aws_subnet.dev1-subnet.id, aws_subnet.dev2-subnet.id]
 
   # If the instance is too small, you will not have enough available NICs to assign IP addresses to
   # all the pods on your instances
-  worker_groups = [
-    {
-      name                          = "dev-worker-group-1"
-      instance_type                 = "t3.medium"
-      asg_min_size                  = 1
-      asg_desired_capacity          = 2
-      asg_max_size                  = 3
-      additional_security_group_ids = [aws_security_group.allow-web-traffic.id]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 50
+    instance_types         = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+    vpc_security_group_ids = [aws_security_group.allow-web-traffic.id]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 2
+      max_size     = 5
+      desired_size = 2
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+      labels = {
+        Environment = "test"
+        GithubRepo  = "terraform-aws-eks"
+        GithubOrg   = "terraform-aws-modules"
+      }
+      taints = {
+        dedicated = {
+          key    = "dedicated"
+          value  = "gpuGroup"
+          effect = "NO_SCHEDULE"
+        }
+      }
+      tags = {
+        ExtraTag = "example"
+      }
     }
-  ]
+  }
 }
+      #additional_security_group_ids = [aws_security_group.allow-web-traffic.id]
+ 
